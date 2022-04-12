28e4ffcee19ea04f15c13f982a2c4e6bb87fca67
Benjamin Rogers <benjrogers48@gmail.com>
2022-04-13 08:44:50 +1000

    added tailwind to project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from "@fortawesome/fontawesome-svg-core";
